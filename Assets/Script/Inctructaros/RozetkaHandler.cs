b6e3ea003c432e2f0622a168cfe036ec94bee23e
ZeGro4 <72697635+ZeGro4@users.noreply.github.com>
2025-12-11 19:57:35 +0300

    Addede animations and USB

---
+++ Assets/Script/Inctructaros/RozetkaHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RozetkaHandler : MonoBehaviour {
+
+	
+	
+	private Animator animator;
+	
+	bool isOn = false;
+	
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (Input.GetKeyDown(KeyCode.E) && !isOn) {
+
+
+            animator.SetBool("On", !animator.GetBool("On"));
+        }
+        //if (Input.GetKeyDown(KeyCode.E) && isOn)
+        //{
+
+        //    animator.SetBool("On", false);
+        //    isOn = false;
+        //}
+    }
+}

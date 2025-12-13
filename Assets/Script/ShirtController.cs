1465fd8c2ea2e80608040518dbbb24a7ae737b9a
ZeGro4 <72697635+ZeGro4@users.noreply.github.com>
2025-12-13 13:49:03 +0300

    added shirt animations

--- Assets/Script/ShirtController.cs
+++ Assets/Script/ShirtController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class ShirtController : MonoBehaviour {
 
 	Animator animator;
-	public Material material;
+	public Material materialRed;
+	public Material materialGreen;
+	public Material materialYellow;
+
+
+	bool isColored= false;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -16,17 +21,51 @@ public class ShirtController : MonoBehaviour {
 
 		if (animator != null)
 		{
-			if (Input.GetKeyDown(KeyCode.J))
+			if (Input.GetKeyDown(KeyCode.R))
 			{
-				animator.SetBool("IsMoving", true);
-				StartCoroutine(ColorShirt());
+				ColoredShirt("red");
+				
 			}
-			
-		}
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                ColoredShirt("green");
+
+            }
+			if (Input.GetKeyDown(KeyCode.Y))
+			{
+                ColoredShirt("yellow");
+            }
+           
+        }
 
 	}
 
-	private IEnumerator ColorShirt()
+	public void ColoredShirt(string color)
+	{
+        
+		if (isColored) {
+			gameObject.transform.position = new Vector3(14.48f, 7.85f, -10.49f);
+
+            animator.SetBool("IsMoving", false);
+        }
+        animator.SetBool("IsMoving", true);
+
+        switch (color) {
+
+			case "red":
+                StartCoroutine(ColorShirt(materialRed));
+				break;
+			case "green":
+				StartCoroutine(ColorShirt(materialGreen));
+				break;
+			case "yellow":
+				StartCoroutine(ColorShirt(materialYellow));
+				break;
+        }
+		isColored = true;
+	}
+
+	private IEnumerator ColorShirt(Material material)
 	{
 		yield return new WaitForSeconds(6f);
 		gameObject.GetComponent<MeshRenderer>().material = material;
